034bbd40ee4ea165bb342d37b4d1220006e23fb9
CTDragon <CTDragon@CTDragon-PC.AYDINDRIL>
2013-09-15 19:50:07 -0400

    BUGFIX: Added try-catch to delete tmp file in case a file is open an cannot be deleted FUNCTIONALITY: Tested differences between compare-merge and compare. Allowed option to compare documents (specifying a last modified by name) TESTING: Ran some tests

--- SFSO/ThisAddIn.cs
+++ SFSO/ThisAddIn.cs
@@ -56,7 +56,8 @@ namespace SFSO
             this.Application.DocumentBeforeSave += new Word.ApplicationEvents4_DocumentBeforeSaveEventHandler(handlers.Application_DocumentBeforeSave);
             this.Application.DocumentBeforeClose += handlers.Application_DocumentBeforeClose;
             this.Application.DocumentChange += Application_DocumentNew;
-            
+            //Globals.ThisAddIn.Application.ActiveDocument.Compare(fileName, googleAuthor, Word.WdCompareTarget.wdCompareTargetCurrent, true, false, false, false);
+            //    (fileName, Word.WdMergeTarget.wdMergeTargetCurrent, true, Word.WdUseFormattingFrom.wdFormattingFromPrompt, false);
             handlers.AddIn_Startup(Globals.ThisAddIn.Application.ActiveDocument, Globals.ThisAddIn.Application.ActiveDocument.CustomDocumentProperties);
         }
 
